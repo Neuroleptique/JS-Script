a9914343d8fb7812b5da2912114121b3c53f436c
neuroleptique <corn.xiao@gmail.com>
2023-08-11 14:18:22 -0400

    Change the name variable and format for readability

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const filename = process.argv[2];
-const name = process.argv[3]
-let greeting = `console.log(Hello ${name})`
+const personName = process.argv[3]
+let greeting = `Hello ${personName}`
 
 fs.writeFile(filename, greeting, (err) => {
   if (err) throw err;
-  console.log(`Data has been written`)
+  console.log("Message has been written")
 })
